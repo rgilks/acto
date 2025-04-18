cbfd2f30f720ffb36a21bb0f7f7915f499fdb10d
rgilks <rob.gilks@gmail.com>
2025-04-19 00:02:59 +0100

    chore: Fix lint warning and optimize CI build speed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import { authOptions } from '@/lib/authOptions';
 // import { NextApiHandler } from 'next'; // Revert this import
 
 // Revert handler definition
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
